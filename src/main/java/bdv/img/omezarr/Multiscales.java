899ae7f9b0596c2181fc1475502aaa61c53151d9
Gabor Kovacs <gabor.kovacs@alleninstitute.org>
2023-03-04 00:32:36 -0500

    Add SharedQueue to ZarrImageLoader.
     * Try gson writer. Complicated to rebase without this.
     * Respect NumFetcherThread settings because it is used in spark.
     * Missing isOpen statement added.
     * Add Closeable interface to ZarrImageLoader.

--- src/main/java/bdv/img/omezarr/Multiscales.java
+++ src/main/java/bdv/img/omezarr/Multiscales.java
@@ -28,10 +28,16 @@
  */
 package bdv.img.omezarr;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
+import org.janelia.saalfeldlab.n5.GsonAttributesParser;
+import org.jcodings.util.Hash;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.*;
 
 /**
  * Class to store OME-zarr json metadata.
@@ -49,12 +55,12 @@ public class Multiscales
     public static final String MULTI_SCALE_KEY = "multiscales";
 
     // Serialisation
-    private String version;
-    private String name;
-    private String type;
-    private Axis[] axes; // from v0.4+ within JSON
-    private Dataset[] datasets;
-    private CoordinateTransformations[] coordinateTransformations; // from v0.4+ within JSON
+    public String version;
+    public String name;
+    public String type;
+    public Axis[] axes; // from v0.4+ within JSON
+    public Dataset[] datasets;
+    public CoordinateTransformations[] coordinateTransformations; // from v0.4+ within JSON
 
     // Runtime
 
@@ -62,7 +68,7 @@ public class Multiscales
     // but in reversed order to accommodate
     // the Java array ordering of the image data.
     private List< Axis > axisList;
-    private int numDimensions;
+    public int numDimensions;
 
     public Multiscales() {
     }
@@ -98,6 +104,9 @@ public class Multiscales
         public String unit;
     }
 
+    /**
+     * Initialize {@code axisList} field with the reversed order entries of {@code axes}.
+     */
     public void init()
     {
         axisList = new ArrayList<Axis>(axes.length);
@@ -162,9 +171,28 @@ public class Multiscales
         return -1;
     }
 
+    /**
+     * Get axes definitions.
+     *
+     * @return The axis instance in this class. The {@code Axis} items are in the java order.
+     */
     public List< Axis > getAxes()
     {
         return axisList;
+    }
+
+    /**
+     * @param newaxes The new axes definition to be set in this class. The {@code Axis} instances are not copied.
+     */
+    public void setAxes(Collection<Axis> newaxes)
+    {
+        axisList.clear();
+        axisList.addAll(newaxes);
+        numDimensions = axisList.size();
+        axes = new Axis[numDimensions];
+        for (int i=numDimensions; i-- >0; ) {
+            axes[i] = axisList.get(i);
+        }
     }
 
     /**
@@ -189,4 +217,19 @@ public class Multiscales
     {
         return numDimensions;
     }
+
+    public static void main(String[] args ) throws IOException {
+        final Multiscales M = new Multiscales();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        final LinkedHashMap<String, JsonElement> map = new LinkedHashMap<>();
+        final HashMap<String, Multiscales> m2 = new HashMap<>();
+        M.datasets = new Dataset[1];
+        M.datasets[0] = new Dataset();
+
+        m2.put(M.MULTI_SCALE_KEY, M);
+        GsonAttributesParser.insertAttributes(map, m2, gson);
+        final BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
+        GsonAttributesParser.writeAttributes(w, map, gson);
+    }
 }
+
